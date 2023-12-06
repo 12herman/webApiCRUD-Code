59bd5e66d6740ac6c198f16883fe8f307a6aa55a
Herman <108892289+12herman@users.noreply.github.com>
2023-12-06 16:11:07 +0530

    Webapi curd full file

---
+++ WebAPione/WebAPIpracticeOne/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIpracticeOne.Models;
+
+namespace WebAPIpracticeOne.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+      public AppDbContext(DbContextOptions options) : base(options) { }
+      public DbSet<StudentDetails> StudentDetails { get; set; }
+    }
+}
